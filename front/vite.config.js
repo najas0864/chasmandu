8a30317987976f444abc42affcd0a0e0a9ff4322
Najas0864 <najas0864.com>
2025-03-10 15:04:02 +0545

    hold

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'chasmandu-ade3.onrender.com', // add the Render host here
+      'localhost',  // (optional) allow localhost for development
+      '0.0.0.0',    // allow all IPs
+    ],
+  },
 })
