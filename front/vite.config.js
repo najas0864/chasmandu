33afb01395dbc89ddd9d4caaf3177f1e9874b8ad
Najas0864 <najas0864.com>
2025-03-15 09:13:13 +0545

    port fixed

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,13 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      'chasmandu-ade3.onrender.com',
-      'localhost',
-      '0.0.0.0',
-    ],
-    port: process.env.PORT || 10000,
+    host: true,
+    strictPort: true,
+    port: 10000,
+    cors: {
+      origin: ['https://chasmandu-ade3.onrender.com'],
+    },
   },
 })
