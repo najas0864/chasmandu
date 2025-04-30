a7b79e6fee7894f8730c4d202f23138b3d2e3803
Najas0864 <najas0864.com>
2025-04-30 13:02:18 +0545

    ok

--- front/src/views/allProducts.jsx
+++ front/src/views/allProducts.jsx
@@ -96,7 +96,7 @@ const AllProducts = () =>{
             <ul className="Filter">
                 {loading?(new Array(8).fill(null).map((_, index) => (<li className="navScalaton" key={index}></li>))):(<>
                     <li className="filterBtn" onClick={!isFilterVisible ? toggleFilter : null}>
-                        <svg viewBox="0 0 24.00 24.00"  height={"2rem"} width={"2rem"} fill="#FFF" xmlns="http://www.w3.org/2000/svg" stroke="#FFF">
+                        <svg viewBox="0 0 24.00 24.00"  height={"2rem"} width={"2rem"} fill="#000" xmlns="http://www.w3.org/2000/svg" stroke="#FFF">
                             <path d="M4 5L10 5M10 5C10 6.10457 10.8954 7 12 7C13.1046 7 14 6.10457 14 5M10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5M14 5L20 5M4 12L16 12M16 12C16 13.1046 16.8954 14 18 14C19.1046 14 20 13.1046 20 12C20 10.8954 19.1046 10 18 10C16.8954 10 16 10.8954 16 12ZM8 19L20 19M8 19C8 17.8954 7.10457 17 6 17C4.89543 17 4 17.8954 4 19C4 20.1046 4.89543 21 6 21C7.10457 21 8 20.1046 8 19Z" stroke="#FFF" strokeWidth="2" strokeLinecap="round" ></path>
                         </svg>
                     </li>
