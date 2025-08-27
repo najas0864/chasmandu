19f7088f004e806a49e958e993595b9ab94fb373
najas0864 <najas0864@example.com>
2025-08-27 14:34:34 +0545

    ok done

--- front/src/views/eyeglass.jsx
+++ front/src/views/eyeglass.jsx
@@ -17,27 +17,25 @@ const Eyeglass = () =>{
             <Nav/>
             <div style={{backgroundImage:"url(/placeholder.svg)",backgroundSize:"cover",backgroundPosition: "center",backgroundRepeat: "no-repeat"}}>
                 <h2>Get appoinment to check your eye</h2>
-                <p>Please select a preferred store date and time below for eye checkup appointment booking.</p>
-                <div style={{display:'flex'}}>
-                    <div>
-                        <span>Book Your Appointment at 
-                            <select name="location" id="location">
-                                <option value="Boudha">Boudha</option>
-                                <option value="Kumaripati">Kumaripati</option>
-                                <option value="Butwal">Butwal</option>
-                                <option value="Pokhara">Pokhara</option>
-                                <option value="Samakhusi">Samakhusi</option>
-                                <option value="Bhaktapur">Bhaktapur</option>
-                            </select>
-                        </span>
-                    </div>
-                    <div>
-                        <span>Select a Date & Time</span>
+                <marquee>Please select a preferred store date and time below for eye checkup appointment booking.</marquee>
+                <div style={{display:'flex', flexDirection:'column', gap:'10px',border:'1px solid #ccc', padding:'10px', width:'fit-content', margin:'auto'}}>
+                    <span>Book Your Appointment at: 
+                        <select name="location" id="location">
+                            <option selected disabled>Select place to get appoited.</option>
+                            <option value="Boudha">Boudha</option>
+                            <option value="Kumaripati">Kumaripati</option>
+                            <option value="Butwal">Butwal</option>
+                            <option value="Pokhara">Pokhara</option>
+                            <option value="Samakhusi">Samakhusi</option>
+                            <option value="Bhaktapur">Bhaktapur</option>
+                        </select>
+                    </span>
+                    <span>Select a Date & Time:
                         <input type="date" name="date" id="date" />
                         <input type="time" name="time" id="time" />
-                    </div>
+                    </span>
+                    <input style={{padding:"1rem .5rem",width:"fit-content", borderRadius:"7px",fontSize:"20px", backgroundColor:"#3300ffff", border:"2px solid #fff",}} type="button" value="Create Appoinment"/>
                 </div>
-                <input type="button" value="Create Appoinment"/>
             </div>
             <Main products={specs} names={title}/>
             <Foot/>
